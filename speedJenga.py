b81e61e166b3cc6d7046df4d55df860c2b9c52ec
Timothy Liu <tliu@shelltechworks.com>
2018-08-08 12:23:57 -0400

    Added basic game logic

--- speedJenga.py
+++ speedJenga.py
@@ -1,25 +1,42 @@
 import RPi.GPIO as GPIO
+import sys
 
-# Red button will be used to end games
-def redButtonCallback(channel):
-    print("Red button was pushed")
+class SpeedJenga:
+    def __init__(self, numPlayers):
+        self.numPlayers = numPlayers
+        self.currentPlayer = 0
 
-# Green button will be used to start games
-def greenButtonCallback(channel):
-    print("Green button was pushed")
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
 
-# White button will be used to signal end of a player's turn and the start of another's turn. 
-def whiteButtonCallback(channel):
-    print("White button was pushed")
+        # Pin 10 will be the red button
+        GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+        GPIO.add_event_detect(10, GPIO.RISING, callback = self.greenButtonCallback, bouncetime=500)
 
-# Pin 10 will be the red button
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(10, GPIO.RISING, callback = redButtonCallback)
+        GPIO.add_event_detect(18, GPIO.RISING, callback = self.whiteButtonCallback)
 
-message = input("Playing Jenga")
+        GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.cleanup()
+        GPIO.add_event_detect(26, GPIO.RISING, callback = self.redButtonCallback)
+
+    # Red button will be used to end games
+    def redButtonCallback(self, channel):
+        print("Red button was pushed")
+        GPIO.cleanup()
+        sys.exit(0)
+
+    # Green button will be used to start games
+    def greenButtonCallback(self, channel):
+        print("Player " + str(self.currentPlayer) + " successfully made a move")
+        self.currentPlayer = (self.currentPlayer + 1) % self.numPlayers
+        print("New current player is now " + str(self.currentPlayer))
+        # TODO: RESET TIMER LOGIC
+
+
+    # White button will be used to signal end of a player's turn and the start of another's turn. 
+    def whiteButtonCallback(self, channel):
+        print("White button was pushed")
+
--- start.py
+++ start.py
@@ -1,7 +1,16 @@
 import tkinter as tk
+from tkinter import simpledialog
+from speedJenga import SpeedJenga
+import keyboard
 
 def playSpeedJenga():
-    tk.messagebox.showinfo("Instructions", "Press the white button once for each player in the game. After that, press the green button to start the game. Press the red button to cancel")
+    #keyboard.KeyboardEntry(root, keysize=5, keycolor="white").pack()
+    numPlayers = simpledialog.askinteger("Enter number of players", "Number of players", parent=root)
+    print(numPlayers)
+    #tk.messagebox.showinfo("Instructions", "Press the white button once for each player in the game. After that, press the green button to start the game. Press the red button to cancel")
+    game = SpeedJenga(numPlayers)
+    while True:
+        continue
 
 
 root = tk.Tk()
