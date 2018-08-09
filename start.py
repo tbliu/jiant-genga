61075d64c33b9223cc2601656d7647bf20813baa
Timothy Liu <tliu@shelltechworks.com>
2018-08-09 16:07:44 -0400

    Added help menu

--- start.py
+++ start.py
@@ -19,6 +19,9 @@ def randomizedJengaCallback(channel):
     GPIO.cleanup()
     return RandomizedSpeedJenga()
 
+def getHelpCallback(channel):
+    tk.messagebox.showinfo("Help", "For both games: \nPress the green button when prompted to start the game.\nPress the red button if you made a successful move.\nPress the white button when the game ends to return to main menu.")
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(GREEN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -27,8 +30,11 @@ GPIO.add_event_detect(GREEN_BUTTON, GPIO.RISING, callback = randomizedJengaCallb
 GPIO.setup(RED_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(RED_BUTTON, GPIO.RISING, callback = speedJengaCallback, bouncetime=500)
 
+GPIO.setup(WHITE_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(WHITE_BUTTON, GPIO.RISING, callback = getHelpCallback, bouncetime=500)
+
 root = tk.Tk()
-label = tk.Label(root, width=768, height=576, text="Press the green button for Randomized Speed Jenga. \nPress the red button for Normal Speed Jenga", font=("Helvetica", 24))
+label = tk.Label(root, width=768, height=576, text="Press the green button for Randomized Speed Jenga. \nPress the red button for Normal Speed Jenga. \nPress the white button for instructions", font=("Helvetica", 24))
 label.pack()
 
 tk.mainloop()
